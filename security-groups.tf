f35c25f608b101da85d9afe77c55878ffb102a36
Genevieve LEsperance <glesperance@pivotal.io>
2018-04-30 17:43:40 -0700

    Create the opsmanager security group.
    - Following these docs: https://docs.pivotal.io/pivotalcf/2-1/customizing/openstack-setup.html#security

--- security-groups.tf
+++ security-groups.tf
@@ -1,26 +1,47 @@
-resource "openstack_compute_secgroup_v2" "internal" {
+resource "openstack_compute_secgroup_v2" "opsmanager" {
   name        = "${var.project}"
   description = "${var.project} security group"
   region      = "${var.region}"
 
   rule {
     ip_protocol = "tcp"
+    from_port   = "22"
+    to_port     = "22"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    ip_protocol = "tcp"
+    from_port   = "80"
+    to_port     = "80"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    ip_protocol = "tcp"
+    from_port   = "443"
+    to_port     = "443"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    ip_protocol = "tcp"
+    from_port   = "25555"
+    to_port     = "25555"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    ip_protocol = "tcp"
     from_port   = "1"
     to_port     = "65535"
-    cidr        = "0.0.0.0/0"
+    self        = "true"
   }
 
   rule {
     ip_protocol = "udp"
     from_port   = "1"
     to_port     = "65535"
-    cidr        = "0.0.0.0/0"
-  }
-
-  rule {
-    ip_protocol = "icmp"
-    from_port   = "-1"
-    to_port     = "-1"
-    cidr        = "0.0.0.0/0"
+    self        = "true"
   }
 }
