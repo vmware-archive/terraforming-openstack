69bc7a0fddcf13bf57d2d5847a78156a792c5bf7
Rowan Jacobs <rojacobs@pivotal.io>
2018-05-07 11:17:59 -0700

    Add back tcp all ports rule from self for om.
    Signed-off-by: Genevieve LEsperance <glesperance@pivotal.io>

--- security-groups.tf
+++ security-groups.tf
@@ -32,6 +32,13 @@ resource "openstack_compute_secgroup_v2" "ops-manager" {
   }
 
   rule {
+    ip_protocol = "tcp"
+    from_port   = "1"
+    to_port     = "65535"
+    self        = "true"
+  }
+
+  rule {
     ip_protocol = "udp"
     from_port   = "1"
     to_port     = "65535"
